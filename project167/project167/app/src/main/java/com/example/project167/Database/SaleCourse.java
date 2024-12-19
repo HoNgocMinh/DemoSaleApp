fc5abd08e21fe100eebceb011d18b79382fad9d3
HoNgocMinh <hongocminh291204@gmail.com>
2024-12-19 11:40:12 +0700

    Thêm chức năng xóa danh mục admin

--- project167/project167/app/src/main/java/com/example/project167/Database/SaleCourse.java
+++ project167/project167/app/src/main/java/com/example/project167/Database/SaleCourse.java
@@ -79,11 +79,6 @@ public class SaleCourse extends SQLiteOpenHelper {
 
         // Tạo bảng reviews
         db.execSQL(CREATE_TABLE_REVIEWS);
-
-        //Khởi tạo data mặc định
-        db = this.getWritableDatabase();
-        BasicCategory(db);
-        BasicCourse(db);
     }
 
     @Override
@@ -188,5 +183,13 @@ public class SaleCourse extends SQLiteOpenHelper {
     public Cursor getCoursesByCategoryId(int categoryId) {
         SQLiteDatabase db = this.getReadableDatabase();
         return db.rawQuery("SELECT * FROM courses WHERE category_id = ?", new String[]{String.valueOf(categoryId)});
+    }
+
+    //Xóa danh mục ra khỏi data
+    public boolean deleteCategory(int categoryId) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        int rowsAffected = db.delete("categories", "category_id = ?", new String[]{String.valueOf(categoryId)});
+        db.close();
+        return rowsAffected > 0; // Trả về true nếu xóa thành công
     }
 }
